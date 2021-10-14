40fe97e42c0b6186e328fc49bf253fcf962a34d4
Pjiwm <Pim.Munne@gmail.com>
2021-10-14 21:23:40 +0200

    finished day2

--- solutions/day2.c
+++ solutions/day2.c
@@ -3,14 +3,22 @@
 #include "../helpers/file_reader.h"
 #include <string.h>
 
+void set_attributes(int* num1, int* num2, char* letter_match, char* password_rules) {
+    char* rules_dup = strdup(password_rules);
+    strtok(rules_dup, " ");
+    *letter_match = strtok(NULL, " ")[0];
+    *num1 = atoi(strtok(rules_dup, "-"));
+    *num2 = atoi(strtok(NULL, "-"));
+}
+
 size_t part1(char* password_rules[], char* passwords[], size_t LENGTH) {
     size_t valid_passport_count = 0;
 
     for (size_t i = 0; i < LENGTH; i++) {
-        strtok(password_rules[i], " ");
-        char letter_match = strtok(NULL, " ")[0];
-        int min = atoi(strtok(password_rules[i], "-"));
-        int max = atoi(strtok(NULL, "-"));
+        int min;
+        int max;
+        char letter_match;
+        set_attributes(&min, &max, &letter_match, password_rules[i]);
 
         size_t match_count = 0;
         for (size_t j = 0; j < strlen(passwords[i]); j++) {
@@ -21,6 +29,24 @@ size_t part1(char* password_rules[], char* passwords[], size_t LENGTH) {
         if (match_count >= min && match_count <= max) {
             valid_passport_count++;
         }
+    }
+    return valid_passport_count;
+}
+
+size_t part2(char* password_rules[], char* passwords[], size_t LENGTH) {
+    size_t valid_passport_count = 0;
+    for (size_t i = 0; i < LENGTH; i++) {
+        int index1;
+        int index2;
+        char letter_match;
+        set_attributes(&index1, &index2, &letter_match, password_rules[i]);
+
+        if (passwords[i][index1] == letter_match ^ passwords[i][index2] == letter_match) {
+            valid_passport_count++;
+            printf("%s: [%s] m: %c %c\n", password_rules[i], passwords[i], passwords[i][index1], passwords[i][index2]);
+
+        }
+
     }
     return valid_passport_count;
 }
@@ -37,11 +63,10 @@ void day2() {
         password_rules[i] = strtok(lines[i], ":");
         passwords[i] = strtok(NULL, ":");
     }
-    // for (int i = 0; i < LENGTH; i++) {
-    //     printf("%s ", password_rules[i]);
-    //     printf("%s\n", passwords[i]);
-    // }
 
     printf("day 2:\n");
     printf("%ld\n", part1(password_rules, passwords, LENGTH));
+
+    printf("%ld\n", part2(password_rules, passwords, LENGTH));
+
 }
