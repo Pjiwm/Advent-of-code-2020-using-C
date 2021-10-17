f50040bc2d6c779b06bca889bca7116c2ba401c4
Pjiwm <Pim.Munne@gmail.com>
2021-10-18 00:02:36 +0200

    added function to get bags from string

--- solutions/day7.c
+++ solutions/day7.c
@@ -4,24 +4,16 @@
 // #include <ctype.h>
 #include <stdbool.h>
 #include "../helpers/file_reader.h"
-// #include "../helpers/linked_list.h"
+#include "../helpers/linked_list.h"
 _Bool is_bag_end(char* str, size_t index) {
-    if(str[index] == ' ' && str[index+1] == 'b' && str[index+2] == 'a' && str[index+3] == 'g') {
+    if (str[index] == ' ' && str[index + 1] == 'b' && str[index + 2] == 'a' && str[index + 3] == 'g') {
         return 1;
     }
-    // char bag[4] = " bag";
-    // for (size_t i = 0; i < 4; i++) {
-    //     if (!str[index + i + 1] == bag[i]) {
-    //         return 1;
-    //     }
-    // }
     return 0;
 }
 
 _Bool is_bag_beginning(char* str, size_t index) {
     if (str[index - 1] == ' ' && str[index - 2] == 'n' && str[index - 3] == 'i' && str[index - 4] == 'a') {
-        printf("bag begins at index: %ld\n", index);
-        printf("bag begins at char: %c\n", str[index]);
         return 1;
     }
     return 0;
@@ -36,26 +28,37 @@ void bag_builder(char* str, size_t begin, size_t end, char* bag) {
 }
 
 size_t find_gold_bags(char* str, char* rules[], size_t LENGTH) {
+    size_t count = (size_t)str[9];
+    for (size_t i = 0; i < LENGTH; i++) {
+        // Loop over line, we start at 2 to skip the number at the beginning of the string/
+        _Bool is_same_bag = 1;
+        for (size_t j = 2; j < strlen(str); j++) {
+            if (str[j] != rules[i][j]) {
+                is_same_bag = 0;
+            }
+        }
+        if (is_same_bag) {
 
+        }
+    }
 }
 
-void rule_loop(char* rule) {
+void get_bags_from_rule(char* rule, Node** bags) {
     char* bag = (char*)malloc(32 * sizeof(char));
     size_t bag_index = 0;
     for (size_t i = 0; i < strlen(rule); i++) {
-        if (is_bag_beginning(rule, i) || (rule[i-2] == ',' && rule[i-1] == ' ')/*&& !is_bag_end(rule, i)*/) {
+        if (is_bag_beginning(rule, i) || (rule[i - 2] == ',' && rule[i - 1] == ' ')) {
             while (!is_bag_end(rule, i + bag_index)) {
-                printf("building bag\n");
                 bag[bag_index] = rule[i + bag_index];
+                printf("added letter: %c\n", rule[i + bag_index]);
                 bag_index++;
             }
+            bag[bag_index] = '\0';
+            insert_tail(bags, strdup(bag));
             bag_index = 0;
-            printf("made bag: %s with l: %ld \n", bag, strlen(bag));
-            // bag = "\0";
+            free(bag);
         }
     }
-
-    // printf("%s", bag);
 }
 
 
@@ -64,13 +67,14 @@ void day7() {
     char* rules[LENGTH];
     file_to_array("inputs/day7.txt", rules);
 
-    // for (size_t i = 0; i < LENGTH; i++) {
-    //     printf("%s\n", rules[i]);
-    // }
     printf("day7:\n");
-    // rule_loop("bags contain 5 dark brown Bags");
     printf("%s\n", rules[0]);
-    rule_loop(rules[0]);
+    // char* bags[] = malloc(500 * sizeof(char));
+    Node* bags = NULL;
+    get_bags_from_rule(rules[1], &bags);
+    for (Node* curr = bags; curr != NULL; curr = curr->next) {
+        // printf("%s l:%ld\n", curr->value, strlen(curr->value));
+    }
 }
 
 
