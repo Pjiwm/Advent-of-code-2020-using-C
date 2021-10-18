d97f07ce9c508878c4731637feb8a010b7becd5d
Pjiwm <Pim.Munne@gmail.com>
2021-10-18 22:59:18 +0200

    added option to pass arg for day through main

--- main.c
+++ main.c
@@ -1,25 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <ctype.h>
 #include "solutions/solutions.h"
 #include "helpers/file_reader.h"
 #include "helpers/linked_list.h"
-
-int main()
-{
-    Node* root = NULL;
-    insert_tail(&root, "s");
-    insert_tail(&root, "sd");
-    insert_tail(&root, "tit");
-    insert_head(&root, "ddd");
-
-    insert_after(root->next, "woo");
-
-    for (Node* curr = root; curr != NULL; curr = curr->next) {
-        printf("%s\n", curr->value);
-    }
-
-    deallocate(&root);
+void run_all() {
     day1();
     day2();
     day3();
@@ -27,6 +12,42 @@ int main()
     day5();
     day6();
     day7();
+}
+
+
+int main(int argc, char** argv) {
+    int8_t day = 0;
+    if (argc > 1) {
+        day = atoi(argv[1]);
+    }
+    switch (day) {
+    case 1:
+        day1();
+        break;
+    case 2:
+        day2();
+        break;
+    case 3:
+        day3();
+        break;
+    case 4:
+        day4();
+        break;
+    case 5:
+        day5();
+        break;
+    case 6:
+        day6();
+        break;
+    case 7:
+        day7();
+        break;
+    default:
+        printf("No specific or invalid argument.\n displaying all days.\n");
+        run_all();
+        break;
+    }
+
     return 0;
 }
 
