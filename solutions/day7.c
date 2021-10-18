782bdb87ff47d894b5b35350d78d97271b3e86c5
Pjiwm <Pim.Munne@gmail.com>
2021-10-18 17:48:37 +0200

    fixed getting bags with nums in find gold bag function

--- solutions/day7.c
+++ solutions/day7.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-// #include <ctype.h>
 #include <stdbool.h>
 #include "../helpers/file_reader.h"
 #include "../helpers/linked_list.h"
+// gold bag has to be fixed because a gold bag can be shiny gold bag, light gold bag etc.
+_Bool is_gold_bag(char* str) {
+    _Bool is_gold_bag = 0;
+    if (str[2] == 'g' && str[3] == 'o' && str[4] == 'l' && str[5] == 'd') {
+        is_gold_bag = 1;
+    }
+    return is_gold_bag;
+}
+
 _Bool is_bag_end(char* str, size_t index) {
     if (str[index] == ' ' && str[index + 1] == 'b' && str[index + 2] == 'a' && str[index + 3] == 'g') {
         return 1;
@@ -38,6 +46,20 @@ char* get_rule_bag(char* str) {
         return_string[i] = str[i];
         i++;
     }
+    return_string[i] = '\0';
+    printf("made bag: %s", return_string);
+    return return_string;
+}
+
+char * get_bag_without_num(char* str) {
+    char* return_string;
+    int i = 2;
+    while(i < strlen(str)) {
+        return_string[i-2] = str[i];
+        i++;
+    }
+    return_string[i-2] = '\0';
+    printf("passing on bag: %s\n", return_string);
     return return_string;
 }
 
@@ -54,7 +76,7 @@ void get_bags_from_rule(char* rule, Node** bags) {
             bag[bag_index] = '\0';
             insert_tail(bags, strdup(bag));
             bag_index = 0;
-            free(bag);
+            // free(bag);
         }
     }
 }
@@ -66,8 +88,9 @@ size_t find_gold_bags(char* bag, char* rules[], size_t LENGTH) {
     for (size_t i = 0; i < LENGTH; i++) {
         // Loop over line, we start at 2 to skip the number at the beginning of the string/
         _Bool is_rule_from_bag = 1;
-        for (size_t j = 2; j < strlen(bag); j++) {
-            if (bag[j] != rules[i][j - 2]) {
+        printf("bag: %s\n", bag);
+        for (size_t j = 0; j < strlen(bag); j++) {
+            if (bag[j] != rules[i][j]) {
                 is_rule_from_bag = 0;
             }
         }
@@ -75,12 +98,13 @@ size_t find_gold_bags(char* bag, char* rules[], size_t LENGTH) {
             get_bags_from_rule(rules[i], &bags);
             for (Node* curr = bags; curr != NULL; curr = curr->next) {
                 size_t bag_count = (size_t)curr->value[0];
-                printf("got the bags\n");
-                gold_bag_count += (bag_count * find_gold_bags(curr->value, rules, LENGTH));
+                if (is_gold_bag(curr->value)) gold_bag_count++;
+                gold_bag_count += (bag_count * find_gold_bags(get_bag_without_num(curr->value), rules, LENGTH));
             }
             deallocate(&bags);
         }
     }
+
     return gold_bag_count;
 }
 
@@ -99,7 +123,7 @@ void day7() {
     char* bag = get_rule_bag(rules[0]);
     printf("X\n");
     printf("%ld\n", find_gold_bags(bag, rules, LENGTH));
-    
+
 }
 
 
